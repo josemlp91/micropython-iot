50d724964fe44218c6d25f341aea4b870e98081a
Jose Miguel <josemlp.montefrio@gmail.com>
2018-04-14 08:43:56 +0200

    test

--- web.py
+++ web.py
@@ -1,12 +1,69 @@
-import picoweb	
+
+from microWebSrv import MicroWebSrv
 from setup import *
 
-app = picoweb.WebApp(__name__)
-
-@app.route("/")
-def index(req, resp):
-    yield from picoweb.start_response(resp)
-    yield from resp.awrite("Hello world from picoweb running on the ESP32")
 
 run_wifi_access_point()
-app.run(debug=True, host = "192.168.4.1")
+
+
+@MicroWebSrv.route('/test')
+def _httpHandlerTestGet(httpClient, httpResponse) :
+	content = """\
+	<!DOCTYPE html>
+	<html lang=en>
+        <head>
+        	<meta charset="UTF-8" />
+            <title>TEST GET</title>
+        </head>
+        <body>
+            <h1>TEST GET</h1>
+            Client IP address = %s
+            <br />
+			<form action="/test" method="post" accept-charset="ISO-8859-1">
+				First name: <input type="text" name="firstname"><br />
+				Last name: <input type="text" name="lastname"><br />
+				<input type="submit" value="Submit">
+			</form>
+        </body>
+    </html>
+	""" % httpClient.GetIPAddr()
+	httpResponse.WriteResponseOk( headers		 = None,
+								  contentType	 = "text/html",
+								  contentCharset = "UTF-8",
+								  content 		 = content )
+
+
+@MicroWebSrv.route('/test', 'POST')
+def _httpHandlerTestPost(httpClient, httpResponse) :
+	formData  = httpClient.ReadRequestPostedFormData()
+	firstname = formData["firstname"]
+	lastname  = formData["lastname"]
+	content   = """\
+	<!DOCTYPE html>
+	<html lang=en>
+		<head>
+			<meta charset="UTF-8" />
+            <title>TEST POST</title>
+        </head>
+        <body>
+            <h1>TEST POST</h1>
+            Firstname = %s<br />
+            Lastname = %s<br />
+        </body>
+    </html>
+	""" % ( MicroWebSrv.HTMLEscape(firstname),
+		    MicroWebSrv.HTMLEscape(lastname) )
+	httpResponse.WriteResponseOk( headers		 = None,
+								  contentType	 = "text/html",
+								  contentCharset = "UTF-8",
+								  content 		 = content )
+
+
+
+# ----------------------------------------------------------------------------
+
+#routeHandlers = [
+#	( "/test",	"GET",	_httpHandlerTestGet ),
+#	( "/test",	"POST",	_httpHandlerTestPost )
+#]
+
